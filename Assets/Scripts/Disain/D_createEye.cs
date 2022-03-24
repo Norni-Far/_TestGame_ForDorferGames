cd4f857d092a1681c104d68a2ce8048edbf5ba9b
Norni-Far <the.somnium.project.games@gmail.com>
2022-03-24 19:36:13 +0300

    pushAlfa

--- Assets/Scripts/Disain/D_createEye.cs
+++ Assets/Scripts/Disain/D_createEye.cs
@@ -15,17 +15,17 @@ public class D_createEye : MonoBehaviour
 
     private IEnumerator CreateDisajn()
     {
-        yield return new WaitForSeconds(18f);
+        yield return new WaitForSeconds(11f);
 
-        for (int i = 0; i < 17; i++)
+        for (int i = 0; i < 11; i++)
         {
             GameObject Inst = Instantiate(prefabEye, transform);
             Inst.transform.position = new Vector3(Random.Range(-spawnCubeX, spawnCubeX), Random.Range(-spawnCubeY, spawnCubeY), 0);
             Inst.transform.eulerAngles = new Vector3(0, 0, Random.Range(0, 360));
-            yield return new WaitForSeconds(Random.Range(40, 81f));
+            yield return new WaitForSeconds(Random.Range(10, 17.1f));
         }
 
-        yield return new WaitForSeconds(40f);
+        yield return new WaitForSeconds(11f);
         GameObject Inst2 = Instantiate(prefabEye, transform);
         Inst2.transform.localScale = new Vector3(1, 1, 1);
         Inst2.transform.position = new Vector3(0, 2, 0);
